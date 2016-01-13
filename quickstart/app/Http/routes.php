337c286217994ee7b1ca9054fe665346bee6f62f
Odhiambo Dormnic <ayimdomnic@outlook.com>
2016-01-13 00:46:11 -0800

    Lesson3:Passing Data to views

--- quickstart/app/Http/routes.php
+++ quickstart/app/Http/routes.php
@@ -14,9 +14,10 @@
 Route::get('/', function () {
     return view('welcome');
 });
-Route::get('/contact', function(){
-    return view('pages.contact');
+Route::get('about', function(){
+    return view('Pages.about')
 });
+
 /*
 |--------------------------------------------------------------------------
 | Application Routes
---
+++ quickstart/resources/views/pages/about.blade.php
@@ -0,0 +1,11 @@
+<!doctype html>
+<html lang="en">
+    <head>
+        <meta charset= "UTF-8">
+        <title> About:{{$name}} </title>
+        
+    </head>
+    <body>
+        <h1>Am Dom!!<h1>
+    </body>
+    </html>
