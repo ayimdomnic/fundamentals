c26ff38df1be40c772ceabc8e28b31c1ad839d62
Odhiambo Dormnic <ayimdomnic@outlook.com>
2016-01-13 02:32:27 -0800

    Setting Up environments and More Blade 101 : this is interesting

--- quickstart/resources/views/layouts/app.blade.php
+++ quickstart/resources/views/layouts/app.blade.php
@@ -1,82 +1,72 @@
 <!DOCTYPE html>
-<html lang="en">
-<head>
+<html lang="en" class="no-js"><head>
+
     <meta charset="utf-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-
-    <title>Laravel</title>
-
-    <!-- Fonts -->
-    <link href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.4.0/css/font-awesome.min.css" rel='stylesheet' type='text/css'>
-    <link href="https://fonts.googleapis.com/css?family=Lato:100,300,400,700" rel='stylesheet' type='text/css'>
-
-    <!-- Styles -->
-    <link href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css" rel="stylesheet">
-    {{-- <link href="{{ elixir('css/app.css') }}" rel="stylesheet"> --}}
-
-    <style>
-        body {
-            font-family: 'Lato';
-        }
-
-        .fa-btn {
-            margin-right: 6px;
-        }
-    </style>
+    <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no">
+    <meta name="format-detection" content="telephone=no" />  
+    <meta name="description" content="">
+    <meta name="author" content="">
+    <title> North Col - Responsive Theme</title>
+    <!-- Google Fonts -->
+    <link href='http://fonts.googleapis.com/css?family=Open+Sans:300italic,400italic,400,300,600,700,800' rel='stylesheet' type='text/css'>
+    <!-- CSS Files -->
+    <link href="assets/css/jquery.fancybox.css?v=2.1.5" rel="stylesheet">
+    <link href="assets/css/helpers/jquery.fancybox-buttons.css?v=1.0.5" rel="stylesheet">
+    <link href="assets/css/helpers/jquery.fancybox-thumbs.css?v=1.0.7" rel="stylesheet">
+    <link href="assets/css/flexslider.css" rel="stylesheet">
+    <link href="assets/css/animations.min.css" rel="stylesheet">
+    <link href="assets/css/carousel.css" rel="stylesheet">
+    <link href="assets/css/owl.carousel.css" rel="stylesheet">
+    <link href="assets/css/bootstrap.min.css" rel="stylesheet">
+    <link href="rs-plugin/css/settings.css" rel="stylesheet" media="screen" />
+    <link href="assets/font-awesome/css/font-awesome.min.css" rel="stylesheet">
+    <link href="assets/css/style.css" rel="stylesheet">
+    <link href="assets/css/responsive.css" rel="stylesheet">
+    <link href="assets/css/vertical-xc.min.css" rel="stylesheet">
+    
 </head>
-<body id="app-layout">
-    <nav class="navbar navbar-default">
-        <div class="container">
-            <div class="navbar-header">
 
-                <!-- Collapsed Hamburger -->
-                <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#app-navbar-collapse">
-                    <span class="sr-only">Toggle Navigation</span>
-                    <span class="icon-bar"></span>
-                    <span class="icon-bar"></span>
-                    <span class="icon-bar"></span>
-                </button>
+<body id="page-top" data-spy="scroll" data-target=".navbar-fixed-top">
 
-                <!-- Branding Image -->
-                <a class="navbar-brand" href="{{ url('/') }}">
-                    Laravel
-                </a>
-            </div>
-
-            <div class="collapse navbar-collapse" id="app-navbar-collapse">
-                <!-- Left Side Of Navbar -->
-                <ul class="nav navbar-nav">
-                    <li><a href="{{ url('/home') }}">Home</a></li>
-                </ul>
-
-                <!-- Right Side Of Navbar -->
-                <ul class="nav navbar-nav navbar-right">
-                    <!-- Authentication Links -->
-                    @if (Auth::guest())
-                        <li><a href="{{ url('/login') }}">Login</a></li>
-                        <li><a href="{{ url('/register') }}">Register</a></li>
-                    @else
-                        <li class="dropdown">
-                            <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-expanded="false">
-                                {{ Auth::user()->name }} <span class="caret"></span>
-                            </a>
-
-                            <ul class="dropdown-menu" role="menu">
-                                <li><a href="{{ url('/logout') }}"><i class="fa fa-btn fa-sign-out"></i>Logout</a></li>
-                            </ul>
-                        </li>
-                    @endif
-                </ul>
-            </div>
-        </div>
-    </nav>
+<!--Preloader-->
+<div id="loader-wrapper">
+    <div id="loader"></div>
+</div>
+   @include('partials.nav')
 
     @yield('content')
+    @include('partials.footer')
 
     <!-- JavaScripts -->
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/2.1.4/jquery.min.js"></script>
-    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/js/bootstrap.min.js"></script>
-    {{-- <script src="{{ elixir('js/app.js') }}"></script> --}}
+    
+<script src="assets/js/jquery-2.1.3.min.js"></script>
+<script src="assets/js/bootstrap.min.js"></script>
+<script src="assets/js/jquery.easing.min.js"></script>
+<script src="assets/js/scrolling-nav.js"></script>
+<script src="assets/js/classie.js"></script>
+<script src="assets/js/cbpAnimatedHeader.min.js"></script>
+<script src="assets/js/appear.min.js" type="text/javascript"></script>
+<script src="assets/js/animations.min.js" type="text/javascript"></script>
+<script src="assets/js/retina-1.1.0.min.js"></script>
+<script src="assets/js/jquery.fancybox.pack.js?v=2.1.5"></script>
+<script src="assets/js/jquery.fancybox-buttons.js?v=1.0.5"></script>
+<script src="assets/js/jquery.fancybox-media.js?v=1.0.6"></script>
+<script src="assets/js/jquery.fancybox-thumbs.js?v=1.0.7"></script>
+<script src="assets/js/jquery.flexslider.min.js"></script>
+<script src="assets/js/jquery.validate.min.js"></script>
+<script src="assets/js/jquery.form.min.js"></script>
+<script src="http://maps.google.com/maps/api/js?sensor=false"></script>
+<script src="assets/js/map.grey.js"></script>
+<script src="assets/js/isotope.pkgd.min.js" type="text/javascript"></script>
+<script src="assets/js/owl.carousel.min.js"></script>
+<script src="assets/js/jquery.parallax-1.1.3.js"></script>
+<script src="assets/js/jquery.countTo.js" type="text/javascript"></script>
+<script src="assets/js/SmoothScroll.js"></script>
+<script src="assets/js/lazy-bootstrap-carousel-v3.js"></script>
+<script src="rs-plugin/js/jquery.themepunch.tools.min.js"></script>
+<script src="rs-plugin/js/jquery.themepunch.revolution.min.js"></script>
+<script src="assets/js/main.js" type="text/javascript"></script>
+
 </body>
 </html>
--- quickstart/resources/views/partials/nav.blade.php
+++ quickstart/resources/views/partials/nav.blade.php
@@ -1,247 +1,45 @@
-<!-- Navigation -->
-   <nav class="navbar mega-menu navbar-fixed-top nav-height" role="navigation">
+ <nav class="navbar navbar-default">
         <div class="container">
-            <!-- Brand and toggle get grouped for better mobile display -->
-            <div class="navbar-header page-scroll">
-                <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#navbar-collapse-1">
-                    <span class="sr-only">Toggle navigation</span>
+            <div class="navbar-header">
+
+                <!-- Collapsed Hamburger -->
+                <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#app-navbar-collapse">
+                    <span class="sr-only">Toggle Navigation</span>
                     <span class="icon-bar"></span>
                     <span class="icon-bar"></span>
                     <span class="icon-bar"></span>
                 </button>
-            <a class="navbar-brand page-scroll" href="#page-top"><img src="assets/img/logo.png" alt=""></a>
+
+                <!-- Branding Image -->
+                <a class="navbar-brand" href="{{ url('/') }}">
+                    Laravel
+                </a>
             </div>
-            <!-- Collect the nav links, forms, and other content for toggling -->
-            <div class="collapse navbar-collapse" id="navbar-collapse-1">
-               <ul class="nav navbar-nav navbar-right">
-                  <!-- Dropdown Mega-Menu 2 Col -->
-                    <li class="dropdown">
-                     <a href="#" class="dropdown-toggle" data-toggle="dropdown">Home</a>
-                       <ul class="dropdown-menu">
-                         <li>
-                           <!-- Content Container -->
-                           <div class="mega-menu-content">
-                            <div class="row">
-                              <ul class="col-sm-12 col-md-6 list-unstyled">
-                                 <li class="mega-menu-title">
-                                  Multi Page
-                                 </li>
-                                 <li><a href="index-main-mp.html">Main Demo</a></li>
-                                 <li><a href="index-fullscreen-mp.html">Fullscreen Slider</a></li>
-                                 <li><a href="index-image-mp.html">Image Background</a></li>
-                                 <li><a href="index-video-mp.html">Video Background</a></li>
-                                 <li><a href="index-parallax-mp.html">Parallax Background</a></li>
-                                 <li><a href="index-textrotator-mp.html">Text Rotator</a></li>
-                              </ul>
-                               
-                              <ul class="col-sm-12 col-md-6 list-unstyled">  
-                                 <li class="mega-menu-title">
-                                 One Page</li>
-                                 <li><a href="index-main-op.html">Main Demo</a></li>
-                                 <li><a href="index-fullscreen-op.html">Fullscreen Slider</a></li>
-                                 <li><a href="index-image-op.html">Image Background</a></li>
-                                 <li><a href="index-video-op.html">Video Background</a></li>
-                                 <li><a href="index-parallax-op.html">Parallax Background</a></li>
-                                 <li><a href="index-textrotator-op.html">Text Rotator</a></li>
-                              </ul>
-                            </div><!--/.row-->
-                           </div><!--/.mega-menu-content-->
-                         </li>
-                       </ul><!--/.dropdown-menu-->
-                    </li><!--/.dropdown-->
-                   <!-- Dropdown Mega-Menu 2 Col End --> 
-                    
-                   <!-- Dropdown Mega-Menu Full Width -->
-                    <li class="dropdown">
-                     <a href="#" class="dropdown-toggle" data-toggle="dropdown">Pages</a>
-                       <ul class="dropdown-menu mega-width">
-                         <li>
-                           <!-- Content Container -->
-                           <div class="mega-menu-content">
-                             <div class="row">
-                              <ul class="col-sm-12 col-md-4 list-unstyled">
-                                 <!--<li class="mega-menu-title">
-                                 Your Title Here
-                                 </li>-->
-                                 <li><a href="about.html">About</a></li>
-                                 <li><a href="about-2.html">About 2</a></li>
-                                 <li><a href="about-3.html">About 3</a></li>
-                                 <li><a href="services.html">Services</a></li>
-                                 <li><a href="services-2.html">Services 2</a></li>
-                                 <li><a href="skills.html">Our Skills</a></li>
-                                 <li><a href="skills-2.html">Our Skills 2</a></li>
-                                 <li><a href="why-us.html">Why Us</a></li>
-                                 
-                               </ul>
-                               
-                               <ul class="col-sm-12 col-md-4 list-unstyled">
-                                 <li><a href="pricing.html">Pricing</a></li>
-                                 <li><a href="team.html">Team</a></li>
-                                 <li><a href="timeline.html">Timeline</a></li>
-                                 <li><a href="faq.html">Faq</a></li>
-                                 <li><a href="clients.html">Clients</a></li>
-                                 <li><a href="blog-news.html">Latest News</a></li>
-                                 <li><a href="contact.html">Contact</a></li>
-                                 <li><a href="contact-2.html">Contact 2</a></li>
-                               </ul>
-                               
-                               <ul class="col-sm-12 col-md-4 list-unstyled">
-                                 <!--<li class="mega-menu-title">
-                                 Your Title Here
-                                 </li>-->
-                                 <li><a href="testimonial.html">Testimonial</a></li>
-                                 <li><a href="project-page.html">Project Page</a></li>
-                                 <li><a href="app-landing.html">App Landing Page</a></li>
-                                 <li><a href="coming-soon.html">Coming Soon</a></li>
-                                 <li><a href="under-construction.html">Under Construction</a></li>
-                                 <li><a href="404.html">404 Error Page</a></li>
-                                 <li><a href="login.html">Login</a></li>
-						         <li><a href="register.html">Register</a></li>
-                               </ul>
-                      
-                             </div><!--/.row-->
-                           </div><!--/.mega-menu-content-->
-                         </li>
-                       </ul><!--/.dropdown-menu-->
-                    </li><!--/.dropdown-->
-                   <!-- Dropdown Mega-Menu Full Width -->
-                    
-                    <!-- Dropdown Menu -->
-                    <li class="dropdown">
-                        <a href="#" class="dropdown-toggle" data-toggle="dropdown">Components</a>
-                        <ul class="dropdown-menu">
-                           <li><a href="accordions.html"><i class="fa fa-plus-circle"></i> Accordions</a></li>
-                           <li><a href="icons.html"><i class="fa fa-heart"></i> Icons Font Awesome</a></li>
-                           <li><a href="icons-2.html"><i class="et icon-heart"></i> Et-line Icons</a></li>
-                           <li><a href="icons-3.html"><i class="xc icon-heart-xc"></i> Xc Icons</a></li>
-						   <li><a href="buttons.html"><i class="fa fa-dot-circle-o"></i> Buttons</a></li>
-						   <li><a href="animations.html"><i class="fa fa-rocket"></i> Animations</a></li>
-                           <li><a href="bars.html"><i class="fa fa-circle-o-notch"></i> Progress Bars</a></li>
-                           <li><a href="typography.html"><i class="fa fa-font"></i> Typography</a></li>
-						</ul>
-                    </li> <!-- /.dropdown -->
-                    <!-- Dropdown Menu End -->
-                    
-                    <!-- Dropdown Menu -->
-                    <li class="dropdown">
-                        <a href="#" class="dropdown-toggle" data-toggle="dropdown">Header</a>
-                        <ul class="dropdown-menu">
-                           <li><a href="header-default.html">Default</a></li>
-                           <li><a href="header-large.html">Large</a></li>
-						   <li><a href="header-default-image-bgr.html">Default Background Image</a></li>
-                           <li><a href="header-large-image-bgr.html">Large Background Image</a></li>
-						   <li><a href="header-slider-revolution.html">Slider Revolution</a></li>
-                           <li><a href="header-slider-revolution-fullscreen.html">Slider Revolution Fullscreen</a></li>
-						</ul>
-                    </li> <!-- /.dropdown -->
-                    <!-- Dropdown Menu End -->
-                    
-                    <!-- Dropdown Submenu -->
-                    <li class="dropdown">
-                        <a href="#" class="dropdown-toggle" data-toggle="dropdown">Portfolio</a>
-                        <ul class="dropdown-menu">
-                           <li class="dropdown-submenu">
-                               <a href="#" class="dropdown-toggle" data-toggle="dropdown">Full Width</a>
-							   <ul class="dropdown-menu nav-sub-left">
-								   <li><a href="portfolio-wide-2col.html">2 Columns</a></li>
-                                   <li><a href="portfolio-wide-3col.html">3 Columns</a></li>
-                                   <li><a href="portfolio-wide-4col.html">4 Columns</a></li>
-						       </ul>
-						   </li>
-                           <li class="dropdown-submenu">
-                               <a href="#" class="dropdown-toggle" data-toggle="dropdown">Full Width (Gutter)</a>
-							   <ul class="dropdown-menu nav-sub-left">
-								   <li><a href="portfolio-wide-gutter-2col.html">2 Columns</a></li>
-                                   <li><a href="portfolio-wide-gutter-3col.html">3 Columns</a></li>
-                                   <li><a href="portfolio-wide-gutter-4col.html">4 Columns</a></li>
-						       </ul>
-						   </li>
-                           <li class="dropdown-submenu">
-                               <a href="#" class="dropdown-toggle" data-toggle="dropdown">Boxed</a>
-							   <ul class="dropdown-menu nav-sub-left">
-								   <li><a href="portfolio-boxed-2col.html">2 Columns</a></li>
-                                   <li><a href="portfolio-boxed-3col.html">3 Columns</a></li>
-                                   <li><a href="portfolio-boxed-4col.html">4 Columns</a></li>
-						       </ul>
-						   </li>    
-                           <li class="dropdown-submenu">
-                               <a href="#" class="dropdown-toggle" data-toggle="dropdown">Boxed (Gutter)</a>
-							   <ul class="dropdown-menu nav-sub-left">
-								   <li><a href="portfolio-boxed-gutter-2col.html">2 Columns</a></li>
-                                   <li><a href="portfolio-boxed-gutter-3col.html">3 Columns</a></li>
-                                   <li><a href="portfolio-boxed-gutter-4col.html">4 Columns</a></li>
-						       </ul>
-						   </li>    
-						   <li class="dropdown-submenu">
-                               <a href="#" class="dropdown-toggle" data-toggle="dropdown">Masonry</a>
-							   <ul class="dropdown-menu nav-sub-left">
-								   <li><a href="portfolio-wide-masonry-2col.html">2 Columns</a></li>
-                                   <li><a href="portfolio-wide-masonry-3col.html">3 Columns</a></li>
-                                   <li><a href="portfolio-wide-masonry-4col.html">4 Columns</a></li>
-						       </ul>
-						   </li>   
-						   <li class="dropdown-submenu">
-                               <a href="#" class="dropdown-toggle" data-toggle="dropdown">Masonry Boxed</a>
-							   <ul class="dropdown-menu nav-sub-left">
-								   <li><a href="portfolio-boxed-masonry-2col.html">2 Columns</a></li>
-                                   <li><a href="portfolio-boxed-masonry-3col.html">3 Columns</a></li>
-                                   <li><a href="portfolio-boxed-masonry-4col.html">4 Columns</a></li>
-						       </ul>
-						   </li>      
-						</ul>
-                    </li> <!-- /.dropdown -->
-                    <!-- Dropdown Submenu End -->
-                    
-                    <!-- Dropdown Menu -->
-                    <li class="dropdown">
-                        <a href="#" class="dropdown-toggle" data-toggle="dropdown">Blog</a>
-                        <ul class="dropdown-menu">
-                           <li><a href="blog-sidebar-left.html">Left Sidebar</a></li>
-                           <li><a href="blog-sidebar-right.html">Right Sidebar</a></li>
-                           <li><a href="blog-fullwidth.html">Fullwidth</a></li>
-                           <li><a href="blog-single-sidebar-left.html">Single Left Sidebar</a></li>
-                           <li><a href="blog-single-sidebar-right.html">Single Right Sidebar</a></li>
-                           <li><a href="blog-single-fullwidth.html">Single Fullwidth</a></li>
-						</ul>
-                    </li> <!-- /.dropdown -->
-                    <!-- Dropdown Menu End -->
-                    
-                    <!-- Dropdown Menu -->
-                    <li class="dropdown">
-                        <a href="#" class="dropdown-toggle" data-toggle="dropdown">Shop</a>
-                        <ul class="dropdown-menu">
-                           <li><a href="shop-2col.html">2 Columns</a></li>
-                           <li><a href="shop-3col.html">3 Columns</a></li>
-                           <li><a href="shop-4col.html">4 Columns</a></li>
-                           <li><a href="shop-single-product.html">Single Product</a></li>
-                           <li><a href="shop-cart.html">Shopping Cart</a></li>
-                        </ul>
-                    </li> <!-- /.dropdown -->
-                    <!-- Dropdown Menu End -->
-                    
-                    <!-- Dropdown Search -->
-                    <li class="dropdown no-caret">
-                        <a href="#" class="dropdown-toggle" data-toggle="dropdown"><i class="fa fa-search"></i>Search</a>
-                        <ul class="dropdown-menu">
-                          <li>
-                            <div class="navbar-search">
-                              <form role="search">               
-                                <div class="input-group">
-                                  <input type="search" id="search" class="form-control"  placeholder="Search..">
-                                  <span class="input-group-btn">
-                                  <button type="submit" class="btn btn-default" value="Search"><i class="fa fa-search"></i></button>
-                                  </span>
-                                </div>      
-                              </form>
-                            </div>
-                          </li>
-                         </ul>
-                     </li> <!-- /.dropdown -->
-                    <!-- Whole Dropdown Menu End -->
-               </ul>
+
+            <div class="collapse navbar-collapse" id="app-navbar-collapse">
+                <!-- Left Side Of Navbar -->
+                <ul class="nav navbar-nav">
+                    <li><a href="{{ url('/home') }}">Home</a></li>
+                </ul>
+
+                <!-- Right Side Of Navbar -->
+                <ul class="nav navbar-nav navbar-right">
+                    <!-- Authentication Links -->
+                    @if (Auth::guest())
+                        <li><a href="{{ url('/login') }}">Login</a></li>
+                        <li><a href="{{ url('/register') }}">Register</a></li>
+                    @else
+                        <li class="dropdown">
+                            <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-expanded="false">
+                                {{ Auth::user()->name }} <span class="caret"></span>
+                            </a>
+
+                            <ul class="dropdown-menu" role="menu">
+                                <li><a href="{{ url('/logout') }}"><i class="fa fa-btn fa-sign-out"></i>Logout</a></li>
+                            </ul>
+                        </li>
+                    @endif
+                </ul>
             </div>
-            <!-- /.navbar-collapse -->
         </div>
-        <!-- /.container -->
     </nav>
-  <!-- navbar-end -->
