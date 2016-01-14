cee88bb51a27684f178ef4b233bedb54f808f7a7
Odhiambo Dormnic <ayimdomnic@outlook.com>
2016-01-14 06:06:03 -0800

    passing data to views, there are several ways (passed)

---
+++ resources/views/pages/about.blade.php
@@ -0,0 +1,13 @@
+<! doctype html>
+<html>
+<head>
+    <title>About me:  </title>
+
+</head>
+<body>
+<h1> About me {{$first}} {{$last}}</h1>
+<p> loreim ipsum hvsdfjvj hjvfhgsgjfvjds hjvfj hkvbfjksdf khbjdsf kjhsfbsd kufkskfhssd lfbjsdf
+
+</p>
+</body>
+</html>
